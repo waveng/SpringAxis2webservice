dc4eb697d2c061509a51a4cc0a46a31afc341254
“jenkins” <git.robot@bozhon.com>
2016-12-27 10:40:11 +0800

    Initial commit

---
+++ src/main/java/com/ws/helloword/service/SayHello.java
@@ -0,0 +1,13 @@
+package com.ws.helloword.service;
+
+/**
+ * Created by justin-Mao on 16-12-27.
+ */
+public class SayHello {
+
+    public String sayHello(){
+        System.out.println("被调用了>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        return "hello";
+    }
+
+}
