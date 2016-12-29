5ee9e7e7a6c9935226fbdc57bbe8ab2a17714aba
justin <543011395@qq.com>
2016-12-29 13:56:34 +0800

    123

--- src/main/java/com/mjc/socketService/clientTest/SocketClient.java
+++ src/main/java/com/mjc/socketService/clientTest/SocketClient.java
@@ -2,6 +2,7 @@ package com.mjc.socketService.clientTest;
 
 import java.io.*;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 /**
  * Created by justin-Mao on 16-12-29.
@@ -9,36 +10,34 @@ import java.net.Socket;
 public class SocketClient {
     public static void main(String[] args) {
         try {
-            //创建socket对象
-            Socket socket = new Socket("localhost",8888);
+            Socket socket = new Socket("localhost", 8888);
 
             //根据输入输出流和服务端连接
-            OutputStream outputStream = socket.getOutputStream();//获取一个输出流,向服务端发送信息
+            OutputStream outputStream = socket.getOutputStream();//获取一个输出流，向服务端发送信息
             PrintWriter printWriter = new PrintWriter(outputStream);//将输出流包装成打印流
-            printWriter.print("hello,service,I'm Justin");
-            socket.shutdownOutput();
-
-            InputStream inputStream = socket.getInputStream();//获取输入流,接受服务端的消息
-            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-
-            String info ="";
-            String temp =null;
-
-            while ((temp=bufferedReader.readLine())!=null){
-                info+=temp;
-                System.out.println("客户端接收服务端发送消息"+info);
+            printWriter.print("服务端你好，我是Balla_兔子");
+            printWriter.flush();
+            socket.shutdownOutput();//关闭输出流
+            InputStream inputStream = socket.getInputStream();//获取一个输入流，接收服务端的信息
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);//包装成字符流，提高效率
+            BufferedReader bufferedReader=new BufferedReader(inputStreamReader);//缓冲区
+            String info = "";
+            String temp = null;//临时变量
+            while ((temp = bufferedReader.readLine()) != null) {
+                info += temp;
+                System.out.println("客户端接收服务端发送信息：" + info);
             }
 
+            //关闭相对应的资源
             bufferedReader.close();
             inputStream.close();
             printWriter.close();
             outputStream.close();
             socket.close();
-
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
 }
